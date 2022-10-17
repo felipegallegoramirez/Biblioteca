999a47db94bc9ec231367c52ac3815fa6437b33c
Toro <erkonz1245@gmail.com>
2022-10-17 18:07:48 -0500

    inputs

--- Biblioteca/src/app/app.module.ts
+++ Biblioteca/src/app/app.module.ts
@@ -5,17 +5,20 @@ import { AppComponent } from './app.component';
 import { GaleriaPlantaComponent } from './galeria-planta/galeria-planta.component';
 import { Routes, RouterModule } from '@angular/router';
 import { VistaPlantasComponent } from './vista-plantas/vista-plantas.component';
+import { InputComponent } from './input/input.component';
 
 const routes: Routes = [
   { path: 'GaleriaPlantas', component: GaleriaPlantaComponent },
   { path: 'VerPlantas', component: VistaPlantasComponent },
+  { path: 'Input', component: InputComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     GaleriaPlantaComponent,
-    VistaPlantasComponent
+    VistaPlantasComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
