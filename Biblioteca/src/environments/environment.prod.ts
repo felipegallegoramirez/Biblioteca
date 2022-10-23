b06a6c91fa943778264732e691b0c3935fddd581
Felipe Gallego <felipegallego600@gmail.com>
2022-10-22 19:03:28 -0500

    Casit

--- Biblioteca/src/environments/environment.prod.ts
+++ Biblioteca/src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCiNd0YHcAFdY5dVSdt_3iD81U0D4RPCjY",
-    authDomain: "galeria-be4c5.firebaseapp.com",
-    projectId: "galeria-be4c5",
-    storageBucket: "galeria-be4c5.appspot.com",
-    messagingSenderId: "48555536789",
-    appId: "1:48555536789:web:331524ec67631f8bb0469c"
+    projectId: 'galeria-be4c5',
+    appId: '1:48555536789:web:331524ec67631f8bb0469c',
+    storageBucket: 'galeria-be4c5.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCiNd0YHcAFdY5dVSdt_3iD81U0D4RPCjY',
+    authDomain: 'galeria-be4c5.firebaseapp.com',
+    messagingSenderId: '48555536789',
   }
 };
